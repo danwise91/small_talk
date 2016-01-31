baf688d2898b366deec474c8cd80df1fe04cdba0
SchuylerMaclay <schuylermaclay@gmail.com>
2016-01-31 15:31:17 -0500

    wrong direction maybe

--- db/migrate/20160131201627_create_table_themes_tips.rb
+++
@@ -1,8 +0,0 @@
-class CreateTableThemesTips < ActiveRecord::Migration
-  def change
-    create_table :table_themes_tips do |t|
-      t.belongs_to :tip, index: true, foreign_key: true
-      t.belongs_to :theme, index: true, foreign_key: true
-    end
-  end
-end
