6418e67c670bb61ba7afd00ec2b106bff67eb6ea
Sam Vary <sam.vary@gmail.com>
2016-02-01 16:44:50 -0500

    added reddit comments viewer box and kristy's refresh button

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get '/twilio', to: 'twilio#index'
   post 'call' => 'twilio#call'
   post 'connect' => 'twilio#connect'
+  post 'themes/new' => 'themes#show'
 
 end
