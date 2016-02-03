16f453bafbe36fd66780b7257a94140f5c483137
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-03 12:24:13 -0500

    hack fb and sessions together

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :posts
   has_many :comments
-  validates_uniqueness_of :email
-  validates_presence_of :email
-  validates_uniqueness_of :name
-  validates_presence_of :name
+  # validates_uniqueness_of :email
+  # validates_presence_of :email
+  # validates_uniqueness_of :name
+  # validates_presence_of :name
 
   def self.from_omniauth(auth)
    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.password = "a"
       user.save!
     end
   end
