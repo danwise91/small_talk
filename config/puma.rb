74c8b7571d36f1864e2f2924cd43811d0f6004b9
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-05 11:35:37 -0500

    revert concurrency settings

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ environment ENV['RACK_ENV'] || 'development'
 #   ActiveRecord::Base.establish_connection
 # end
 #
-on_worker_boot do
-  # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
-  ActiveRecord::Base.establish_connection
-end
+# on_worker_boot do
+#   # Valid on Rails 4.1+ using the `config/database.yml` method of setting `pool` size
+#   ActiveRecord::Base.establish_connection
+# end
