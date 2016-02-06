3643bf72f8b2fe06de354cb760bbb40b6249f63a
kjordan88 <kristy.jordan88@gmail.com>
2016-02-06 16:07:38 -0500

    adds ajax to save tip button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,17 +15,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-  if @user.save
-    session[:user_id] = @user.id
-    redirect_to '/', notice: "Thank you for signing up!"
-  else
-    render :new
-  end
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/', notice: "Thank you for signing up!"
+    else
+      render :new
+    end
   end
 
   def edit
     tip = Tip.find(params[:id])
     current_user.tips << tip
+    respond_to do |format|
+      format.js { }
+      format.html { redirect_to root_url  }
+    end
   end
 
   def delete_tip
