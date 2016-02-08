8e78ee7812682e9e77c58d931f04892c6a094326
Dan Wise <dwise2@oswego.edu>
2016-02-08 12:06:08 -0500

    added ajax to tips

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -8,7 +8,16 @@ class ThemesController < ApplicationController
     @theme = Theme.find(params[:id])
     @tips = Tip.all
     @content = RedditScraper.new(@theme.id).scrape
+    respond_to do |format|
+      # binding.pry
+      format.js { }
+     format.html { }
+    end
   end
 
+  def refresh
+    @tips = Tip.all
+    @content = RedditScraper.new(params[:id].to_i).scrape
+  end
 
 end
