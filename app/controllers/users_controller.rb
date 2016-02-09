84cb7cb98b98ec80ba53bac85a124104ab10baa9
kjordan88 <kristy.jordan88@gmail.com>
2016-02-09 13:42:40 -0500

    adds ajax to signup modal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,15 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/', notice: "Thank you for signing up!"
+      respond_to do |format|
+        format.js { render inline: "location.reload();" }
+        format.html { redirect_to root_url  }
+      end
     else
-      render :new
+      respond_to do |format|
+        format.js { }
+        format.html { redirect_to root_url  }
+      end
     end
   end
 
