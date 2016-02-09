52f3ac0e42ac00eec38803c1bcd86fced4e4ee41
kjordan88 <kristy.jordan88@gmail.com>
2016-02-09 09:22:57 -0500

    gets rid of hello user message on home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
       @user = User.find_by(email: params[:user][:email])
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
-        redirect_to '/', notice: "Hello, #{current_user.name}!"
+        redirect_to '/'
       else
 
         @user = User.new
