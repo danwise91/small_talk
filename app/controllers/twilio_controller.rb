548211f6a26b2c1cf92d4e180d7f4233c2155fcc
Schuyler Maclay <schuylermaclay@gmail.com>
2016-02-17 15:12:27 -0500

    modest twilio refactor

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -3,23 +3,21 @@ class TwilioController < ApplicationController
   # Before we allow the incoming request to connect, verify
   # that it is a Twilio request
   protect_from_forgery except: :connect
-  before_filter :authenticate_twilio_request, :only => [
-    :connect
-  ]
+  before_filter :authenticate_twilio_request, :only => [:connect]
 
+  # Define our Twilio credentials as instance variables for later use
   @@twilio_sid = ENV['TWILIO_ACCOUNT_SID']
   @@twilio_token = ENV['TWILIO_AUTH_TOKEN']
   @@twilio_number = ENV['TWILIO_NUMBER']
   @@twilio_connect_url = ENV['CONNECT_URL']
 
-  # Define our Twilio credentials as instance variables for later use
 
   # Render home page
   def index
   	render 'index'
   end
 
-  # Hande a POST from our web form and connect a call via REST API
+  # Handle a POST from our web form and connect a call via REST API
   def call
     time = params[:time].to_f
     contact = Contact.new
@@ -28,27 +26,10 @@ class TwilioController < ApplicationController
     # Validate contact
       if true
       MakeCallJob.set(wait_until: time.minutes.from_now).perform_later(contact.phone, @@twilio_connect_url)
-      # MakeCallJob.perform_later(contact.phone, dummy_url)
-    # if contact.valid?
-
-      # @client = Twilio::REST::Client.new @@twilio_sid, @@twilio_token
-      # # Connect an outbound call to the number submitted
-      # @call = @client.account.calls.create(
-      #   :from => @@twilio_number,
-      #   :to => contact.phone,
-      #   # :url => "http://161464a.ngrok.com/connect"
-      #   :url => connect_url
-      #
-      #   # Fetch instructions from this URL when the call connects
-      #   # :url => dummy_url # Fetch instructions from this URL when the call connects
-      # )
-
-
       # Lets respond to the ajax call with some positive reinforcement
       @msg = { :message => 'Phone call incoming!', :status => 'ok' }
 
     else
-
       # Oops there was an error, lets return the validation errors
       puts  contact.errors.full_messages,
       @msg = { :message => contact.errors.full_messages, :status => 'ok' }
@@ -61,6 +42,7 @@ class TwilioController < ApplicationController
   # This URL contains instructions for the call that is connected with a lead
   # that is using the web form.  These instructions are used either for a
   # direct call to our Twilio number (the mobile use case) or
+
   def connect
 
     # Our response to this request will be an XML document in the "TwiML"
