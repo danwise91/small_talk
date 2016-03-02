5044f56add3f8fcd15a68f361ec6d121a42ecc00
Sam Vary <sam.vary@gmail.com>
2016-03-02 10:55:17 -0500

    changed the theme page to return 5 conversation starters

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -10,11 +10,7 @@ class ThemesController < ApplicationController
     @theme = Theme.find(params[:id])
     @tips = Tip.all
 
-    if @theme.id == 1
-      @content = HackerScraper.new.scrape
-    else
-      @content = RedditScraper.new(@theme.id).scrape
-    end
+    scrape_conversation_starters
 
     respond_to do |format|
       format.js { }
@@ -27,11 +23,16 @@ class ThemesController < ApplicationController
     @tips = Tip.all
 
     @theme = Theme.find(params[:id])
+
+    scrape_conversation_starters
+  end
+
+  def scrape_conversation_starters
     if @theme.id == 1
       @content = HackerScraper.new.scrape
     else
       @content = RedditScraper.new(@theme.id).scrape
+      @articles = @content.to_a.sample(5).to_h
     end
-
   end
 end
